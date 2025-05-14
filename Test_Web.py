179ee9785ab1490101c7656e2053e82ebb419e02
JLCourty <163395133+JLCourty@users.noreply.github.com>
2025-05-14 16:44:11 +0200

    Add files via upload

--- Test_Web.py
+++ Test_Web.py
@@ -17,7 +17,9 @@ def Get_tout(x_code_valeur, x_nom_valeur, x_date_jour, x_qte, x_currency):
     if x_code_valeur:
         stock = yf.Ticker(x_code_valeur)
         info = stock.info
+
         t_prix = info.get("currentPrice")
+        t_prix=t_prix*x_qte
         t_ouverture = info.get("open")
 
         # Calcul de la variation journalière (%)
@@ -30,7 +32,7 @@ def Get_tout(x_code_valeur, x_nom_valeur, x_date_jour, x_qte, x_currency):
         liste_donnees.append([
             x_date_jour,
             x_nom_valeur,
-            t_prix*qte_valeur,
+            t_prix,
             variation_jour,         # ✅ Nouvelle colonne ajoutée ici
             x_qte,x_currency   ])
     else:
