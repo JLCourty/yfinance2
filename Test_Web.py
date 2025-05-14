9d6f4a9172be0800c595510f8d5ea76b166dc54a
JLCourty <163395133+JLCourty@users.noreply.github.com>
2025-05-14 16:22:42 +0200

    Add files via upload

--- Test_Web.py
+++ Test_Web.py
@@ -13,11 +13,10 @@ st.set_page_config(layout="wide")
 #st.title("📈 Mon Application Boursière 12:36")
 
 #MA FONCTION GET TOUT
-def Get_tout(x_code_valeur, x_nom_valeur, x_date_jour, x_cours_1janv, x_cours_1mai, mt_31dec, x_qte, x_currency):
+def Get_tout(x_code_valeur, x_nom_valeur, x_date_jour, x_qte, x_currency):
     if x_code_valeur:
         stock = yf.Ticker(x_code_valeur)
         info = stock.info
-
         t_prix = info.get("currentPrice")
         t_ouverture = info.get("open")
 
@@ -33,45 +32,36 @@ def Get_tout(x_code_valeur, x_nom_valeur, x_date_jour, x_cours_1janv, x_cours_1m
             x_nom_valeur,
             t_prix,
             variation_jour,         # ✅ Nouvelle colonne ajoutée ici
-            x_qte,
-            x_cours_1janv,
-            x_cours_1mai,
-            mt_31dec,
-            x_currency
-        ])
+            x_qte,x_currency   ])
     else:
         st.warning(f"Le ticker n’a pas été trouvé : {x_code_valeur}")
 
 
-
-
-
-
 #LANCER LA FONCTION UNIQUE
-Get_tout('FR0000120404','ACCOR',          x_date_jour,47.39  ,43.15  , 12000 ,214 ,1)
-Get_tout('NL0000235190','AIRBUS',         x_date_jour,154.78 ,147.14 , 19000 ,95  ,1)
-Get_tout('GOOGL'       ,'ALPHABET',       x_date_jour,182.86 ,140.52 , 15000 ,79  , x_cours_dollar)
-Get_tout('US0231351067','AMAZON',         x_date_jour,211.93 ,163.19 , 12000 ,52  , x_cours_dollar)
-Get_tout('NL0010273215','ASML',           x_date_jour,678.7  ,582.51 , 15000 ,18  ,1)
-Get_tout('US11135F1012','BROADCOM',       x_date_jour,223.96 ,170.31 , 18000 ,73  , x_cours_dollar)
-Get_tout('DE0005810055','DEUTSCHE BORSE', x_date_jour,223.60 ,283.51 , 14000 ,42  ,1)
-Get_tout('FR0000052292','HERMES',         x_date_jour,2409.60,2391.01, 12000 ,4   ,1)
-Get_tout('ES0144580Y14','IBERDROLA',      x_date_jour,13.58  ,15.87  , 13000 ,712 ,1)
-Get_tout('IT0003856405','LEONARDO',       x_date_jour,49.69  ,45.81  , 8000 ,142  , 1)
-Get_tout('US5949181045','MICROSOFT',      x_date_jour,407.17 ,349.76 , 25000 ,48  , x_cours_dollar)
-Get_tout('US64110L1061','NETFLIX',        x_date_jour,820.01 ,1001.43, 12000 ,10  , x_cours_dollar)
-Get_tout('US67066G1040','NVDIA',          x_date_jour,133.31 ,96.38  , 22000 ,160 , x_cours_dollar)
-Get_tout('US6974351057','PALO ALTO',      x_date_jour,191.02 ,165.41 , 11000 ,56  , x_cours_dollar)
-Get_tout('DE0007030009','RHEINMETALL',    x_date_jour,1196.01,1499.01, 12000 ,5   ,1)
-Get_tout('US79466L3024','SALESFORCE',     x_date_jour,322.96 ,237.00 , 14000 ,46  , x_cours_dollar)
-Get_tout('FR0000121329','THALES',         x_date_jour,262.93 ,244.90 , 7000 ,24  ,1)
-Get_tout('FR0000120271','TOTAL ENERGIES', x_date_jour,53.37  ,51.16  , 7000  ,111 ,1)
-Get_tout('US92826C8394','VISA',           x_date_jour,305.29 ,305.73 , 14000 ,40  , x_cours_dollar)
-Get_tout('FR0007054358','_ETF STOXX 50',  x_date_jour,53.33  ,56.41  , 105000,1543,1)
-Get_tout('FR0010315770','_ ETF MSCI' ,    x_date_jour,359.41 ,318.64 , 112000,305 ,1)      #VALEUR US FOURNIE EN EUROS
-Get_tout('LU1829221024','_ ETF NASDAQ',   x_date_jour,82.73  ,69.06  , 12000 ,130 ,1)      #VALEUR US FOURNIE EN EUROS
+Get_tout('FR0000120404','ACCOR',          x_date_jour ,214 ,1)
+Get_tout('NL0000235190','AIRBUS',         x_date_jour ,95  ,1)
+Get_tout('GOOGL'       ,'ALPHABET',       x_date_jour,79  , x_cours_dollar)
+Get_tout('US0231351067','AMAZON',         x_date_jour,52  , x_cours_dollar)
+Get_tout('NL0010273215','ASML',           x_date_jour ,18  ,1)
+Get_tout('US11135F1012','BROADCOM',       x_date_jour,73  , x_cours_dollar)
+Get_tout('DE0005810055','DEUTSCHE BORSE', x_date_jour,42  ,1)
+Get_tout('FR0000052292','HERMES',         x_date_jour,4   ,1)
+Get_tout('ES0144580Y14','IBERDROLA',      x_date_jour,712 ,1)
+Get_tout('IT0003856405','LEONARDO',       x_date_jour,142  , 1)
+Get_tout('US5949181045','MICROSOFT',      x_date_jour ,48  , x_cours_dollar)
+Get_tout('US64110L1061','NETFLIX',        x_date_jour ,10  , x_cours_dollar)
+Get_tout('US67066G1040','NVDIA',          x_date_jour,160 , x_cours_dollar)
+Get_tout('US6974351057','PALO ALTO',      x_date_jour,56  , x_cours_dollar)
+Get_tout('DE0007030009','RHEINMETALL',    x_date_jour, 12000 ,5   ,1)
+Get_tout('US79466L3024','SALESFORCE',     x_date_jour ,46  , x_cours_dollar)
+Get_tout('FR0000121329','THALES',         x_date_jour, 7000 ,24  ,1)
+Get_tout('FR0000120271','TOTAL ENERGIES', x_date_jour,  ,111 ,1)
+Get_tout('US92826C8394','VISA',           x_date_jour,40  , x_cours_dollar)
+Get_tout('FR0007054358','_ETF STOXX 50',  x_date_jour,1543,1)
+Get_tout('FR0010315770','_ ETF MSCI' ,    x_date_jour,305 ,1)      #VALEUR US FOURNIE EN EUROS
+Get_tout('LU1829221024','_ ETF NASDAQ',   x_date_jour,130 ,1)      #VALEUR US FOURNIE EN EUROS
 
 # À la fin : convertir en DataFrame et afficher
-columns = [ "Date", "Valeur", "Prix actuel", "variation_jour", "Quantité", "Cours 1er janv", "Cours 1er mai", "Montant 31 déc", "Devise"]
+columns = [ "Date", "Valeur", "Prix actuel", "variation_jour", "Quantité", "Devise"]
 df = pd.DataFrame(liste_donnees, columns=columns)
 st.table(df)
