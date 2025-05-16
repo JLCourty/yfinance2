dfac1a81a5b08716d6889a773631df8cac574b43
JLCourty <163395133+JLCourty@users.noreply.github.com>
2025-05-16 10:55:05 +0200

    Add files via upload

--- Main_Web.py
+++ Main_Web.py
@@ -100,13 +100,12 @@ df_sorted = df.sort_values(by="Progression", ascending=False).reset_index(drop=T
 total_prix = df["Montant"].sum()
 total_prog = df["Progression"].sum()
 
-
 #AFFICHER LES TOTAUX
 if total_prog > 0:
 #    st.markdown("### Total : " + format_euro(total_prix + 131619) + " Gains : " + format_euro(total_prog))
 #    st.markdown("### Le "+x_date_jour + " à " + t_heure_actuelle)
     st.markdown(     f"<p style='margin-top: 0; margin-bottom: 5px; font-size: 32px;'><strong>Total :</strong> {format_euro(total_prix + 131619) } &nbsp;&nbsp;"
-                     f" <strong>Gains :</strong> {format_euro(total_prog) + "<br>"+ x_date_jour +"-" + t_heure_actuelle    }</p>",   unsafe_allow_html=True )
+                     f" <strong>Gains :</strong> {format_euro(total_prog) + "<br> Le "+ x_date_jour +" à " + t_heure_actuelle    }</p>",   unsafe_allow_html=True )
 
 else:
     st.markdown("### Total : " + format_euro(total_prix + 131619) + " Pertes : " + format_euro(total_prog)+"   -"+x_date_jour +"-" + t_heure_actuelle)
