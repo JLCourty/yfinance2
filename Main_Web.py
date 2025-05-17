649677e56b4a437d731c43b15c59c0b90d97d6ae
JLCourty <163395133+JLCourty@users.noreply.github.com>
2025-05-17 12:06:12 +0200

    Add files via upload

--- Main_Web.py
+++ Main_Web.py
@@ -11,20 +11,14 @@ import yfinance as yf
 #DEFINIR LES RESERVES
 t_reserves = 132846
 
-
 # 📌 Fonctions utilitaires
 def format_euro(val):
     return f"{val:,.2f} €".replace(",", " ").replace(".", ",")
 
-
-
 # 🔹 Variables supplémentaires
 date_jour = pd.Timestamp.today()
-
-
 x_date_jour = datetime.now().strftime("%d/%m/%Y")
 t_heure_actuelle = datetime.now().strftime("%H:%M")
-
 
 # 🔹 Exemple de données
 liste_donnees = []
@@ -40,6 +34,7 @@ x_cours_dollar = round(usd_eur_data.history(period="1d")["Close"].iloc[-1], 4)
 
 # Fonction pour chaque ligne
 def Get_tout(x_code_valeur, x_nom_valeur, x_date_jour, x_qte, x_currency):
+
     if x_code_valeur:
         x_ticker = yf.Ticker(x_code_valeur)
         data = x_ticker.history(start="2025-05-11")['Close']
@@ -66,8 +61,8 @@ Get_tout('GOOGL', 'ALPHABET', x_date_jour, 79, x_cours_dollar)
 Get_tout('US0231351067', 'AMAZON', x_date_jour, 52, x_cours_dollar)
 Get_tout('NL0010273215', 'ASML', x_date_jour, 18, 1)
 Get_tout('US11135F1012', 'BROADCOM', x_date_jour, 73, x_cours_dollar)
-#Get_tout('DE0005810055', 'DEUTSCHE BORSE', x_date_jour, 42, 1)
-#Get_tout('FR0000052292', 'HERMES', x_date_jour, 4, 1)
+Get_tout('DE0005810055', 'DEUTSCHE BORSE', x_date_jour, 42, 1)
+Get_tout('FR0000052292', 'HERMES', x_date_jour, 4, 1)
 Get_tout('ES0144580Y14', 'IBERDROLA', x_date_jour, 712, 1)
 Get_tout('IT0003856405', 'LEONARDO', x_date_jour, 142, 1)
 Get_tout('US5949181045', 'MICROSOFT', x_date_jour, 48, x_cours_dollar)
@@ -108,8 +103,7 @@ else:
         f"<p style='font-size: 24px;'>Total : {format_euro(total_prix + t_reserves)} - "
         f"<span style='color: red;'>Pertes : {format_euro(total_prog)}</span> - "
         f"{x_date_jour} - {t_heure_actuelle}</p>",
-        unsafe_allow_html=True
-    )
+        unsafe_allow_html=True    )
 
 # 🔸 Style JS conditionnel pour la progression
 cell_style_js = JsCode("""
